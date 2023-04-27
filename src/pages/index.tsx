a47bda9c855374630adf5fe2de90f27ba9db877c
enkhnairamdal <enkhnairamdalbatmagnai@gmail.com>
2023-04-27 15:36:19 +0800

    client is done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import Example from '../components/example';
+import { useState } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const [name, setName] = useState<string>('Bat');
+  function onEdit(e: string) {
+    setName(e);
+  }
   return (
     <div>
       <h1>Home page</h1>
+      <Example name={name} age={20} handleClick={onEdit} />
     </div>
-  )
+  );
 }
