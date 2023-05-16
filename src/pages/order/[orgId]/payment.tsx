c2ecf46f8f0796a8fb0e6f9ef4bb64e85ebdb2da
Namuun <mit.irnitu@gmail.com>
2023-05-16 15:28:24 +0800

    wrapped with mainlayout and provider

--- src/pages/order/[orgId]/payment.tsx
+++ src/pages/order/[orgId]/payment.tsx
@@ -1,7 +1,10 @@
+import { MainLayout } from '@/components/MainLayout';
+import { UserContext, UserProvider } from '@/context/userProvider';
 import { OrgInfo, orderInfo, selectedDateInfo } from '@/pages/atoms';
 import axios from 'axios';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import React, { useContext } from 'react';
 import {
   AiOutlineArrowLeft,
   AiOutlineArrowRight,
@@ -14,7 +17,8 @@ export default function Payment() {
   const UserSelectedService = useRecoilValue(orderInfo);
   const selectedSPid = useRecoilValue(OrgInfo);
   const selectedDate = useRecoilValue(selectedDateInfo);
-
+  const user = useContext<any>(UserContext);
+  console.log({ user });
   function sendOrder() {
     axios
       .post(`${process.env.NEXT_PUBLIC_REACT_APP_API_URL}/order`, {
@@ -29,26 +33,31 @@ export default function Payment() {
   }
   return (
     <>
-      <h1>Payment</h1>
-      <div className="w-[100%] h-[80px] border-2 border-black mx-auto mt-10 flex justify-center gap-4 items-center">
-        <Link
-          href={`/order/${orgId}/summary`}
-          className="rounded   bg-blue-500 w-[100px] h-[40px] text-white  flex justify-center items-center  "
-        >
-          Back
-        </Link>
-        <Link href={`/`}>
-          <AiOutlineHome className="w-[30px] h-[30px] " type="button" />
-        </Link>
-        <Link
-          className="rounded  bg-blue-500 w-[100px] h-[40px] text-white  flex justify-center items-center  "
-          href={`/order/${orgId}/payment`}
-          type="button"
-          onClick={() => sendOrder()}
-        >
-          Төлбөр
-        </Link>
-      </div>
+      <UserProvider>
+        <MainLayout>
+          <h1>Payment</h1>
+          <h1>{user?.phoneNumber}</h1>
+          <div className="w-[100%] h-[80px] border-2 border-black mx-auto mt-10 flex justify-center gap-4 items-center">
+            <Link
+              href={`/order/${orgId}/summary`}
+              className="rounded   bg-blue-500 w-[100px] h-[40px] text-white  flex justify-center items-center  "
+            >
+              Back
+            </Link>
+            <Link href={`/`}>
+              <AiOutlineHome className="w-[30px] h-[30px] " type="button" />
+            </Link>
+            <Link
+              className="rounded  bg-blue-500 w-[100px] h-[40px] text-white  flex justify-center items-center  "
+              href={`/order/${orgId}/payment`}
+              type="button"
+              onClick={() => sendOrder()}
+            >
+              Төлбөр
+            </Link>
+          </div>
+        </MainLayout>
+      </UserProvider>
     </>
   );
 }
--- src/pages/order/[orgId]/summary.tsx
+++ src/pages/order/[orgId]/summary.tsx
@@ -11,6 +11,8 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { IoLocationOutline } from 'react-icons/io5';
 import { FiClock } from 'react-icons/fi';
+import { UserProvider } from '@/context/userProvider';
+import { MainLayout } from '@/components/MainLayout';
 export default function Summary() {
   const UserSelectedService = useRecoilValue(orderInfo);
   const selectedSPid = useRecoilValue(OrgInfo);
@@ -38,58 +40,62 @@ export default function Summary() {
   console.log(sP, 'sss');
   return (
     <>
-      <h1>Summary</h1>
-      <div className="w-[400px] h-[360px] mx-auto bg-zinc-50 mt-10 rounded">
-        <div className="w-[90%]  divide-y  divide-slate-200 h-screen mx-auto">
-          <h5 className="font-semibold text-[30px] ml-10 p-2">
-            Booking Summury
-          </h5>
-          <div className="w-[100%] p-2 flex gap-6">
-            <IoLocationOutline className="w-6 h-6 mt-2 text-slate-500" />
-            <div>
-              <h5 className="font-bold">{sP.name}</h5>
-              <p className="text-slate-500">{sP.address?.extraAddress}</p>
-            </div>
-          </div>
-          <div className="p-2 flex gap-8 ">
-            <FiClock className="w-5 h-5 ml-1 mt-3" />
-            <div>
-              <h5 className="font-semibold">{selectedDate[0]}</h5>
-              <p className="text-slate-500 ">{selectedDate[1]}</p>
+      <UserProvider>
+        <MainLayout>
+          <h1>Summary</h1>
+          <div className="w-[400px] h-[360px] mx-auto bg-zinc-50 mt-10 rounded">
+            <div className="w-[90%]  divide-y  divide-slate-200 h-screen mx-auto">
+              <h5 className="font-semibold text-[30px] ml-10 p-2">
+                Booking Summury
+              </h5>
+              <div className="w-[100%] p-2 flex gap-6">
+                <IoLocationOutline className="w-6 h-6 mt-2 text-slate-500" />
+                <div>
+                  <h5 className="font-bold">{sP.name}</h5>
+                  <p className="text-slate-500">{sP.address?.extraAddress}</p>
+                </div>
+              </div>
+              <div className="p-2 flex gap-8 ">
+                <FiClock className="w-5 h-5 ml-1 mt-3" />
+                <div>
+                  <h5 className="font-semibold">{selectedDate[0]}</h5>
+                  <p className="text-slate-500 ">{selectedDate[1]}</p>
+                </div>
+              </div>
+              <div>
+                <h5 className="flex justify-between p-2  mt-5 mb-5 font-semibold">
+                  <span>{UserSelectedService.name}</span>
+                  <span>{UserSelectedService.price}₮</span>
+                </h5>
+              </div>
+              <div className="flex justify-between p-2 font-semibold pt-5 ">
+                <span>Total</span>
+                <span>{UserSelectedService.price}₮</span>
+              </div>
             </div>
           </div>
           <div>
-            <h5 className="flex justify-between p-2  mt-5 mb-5 font-semibold">
-              <span>{UserSelectedService.name}</span>
-              <span>{UserSelectedService.price}₮</span>
-            </h5>
+            <div className="w-[100%] h-[80px] border-2 border-black mx-auto mt-10 flex justify-center gap-4 items-center">
+              <Link
+                href={`/order/${orgId}/calendar`}
+                className="rounded   bg-blue-500 w-[100px] h-[40px] text-white  flex justify-center items-center  "
+              >
+                Back
+              </Link>
+              <Link href={`/`}>
+                <AiOutlineHome className="w-[30px] h-[30px] " type="button" />
+              </Link>
+              <Link
+                className="rounded  bg-blue-500 w-[100px] h-[40px] text-white  flex justify-center items-center  "
+                href={`/order/${orgId}/payment`}
+                type="button"
+              >
+                Цаг захиалах
+              </Link>
+            </div>
           </div>
-          <div className="flex justify-between p-2 font-semibold pt-5 ">
-            <span>Total</span>
-            <span>{UserSelectedService.price}₮</span>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div className="w-[100%] h-[80px] border-2 border-black mx-auto mt-10 flex justify-center gap-4 items-center">
-          <Link
-            href={`/order/${orgId}/calendar`}
-            className="rounded   bg-blue-500 w-[100px] h-[40px] text-white  flex justify-center items-center  "
-          >
-            Back
-          </Link>
-          <Link href={`/`}>
-            <AiOutlineHome className="w-[30px] h-[30px] " type="button" />
-          </Link>
-          <Link
-            className="rounded  bg-blue-500 w-[100px] h-[40px] text-white  flex justify-center items-center  "
-            href={`/order/${orgId}/payment`}
-            type="button"
-          >
-            Цаг захиалах
-          </Link>
-        </div>
-      </div>
+        </MainLayout>
+      </UserProvider>
     </>
   );
 }
