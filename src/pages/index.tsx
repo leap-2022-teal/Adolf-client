f160cd24e33779dc947903286a6bff70061e28d4
Evening <evening@1-14.local>
2023-05-18 17:59:35 +0800

    authentication is done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import HomePage from '@/components/home';
 import { MainLayout } from '@/components/MainLayout';
 import { UserProvider } from '@/context/userProvider';
 import { Inter } from 'next/font/google';
+import Login from './login';
 
 const inter = Inter({ subsets: ['latin'] });
 
