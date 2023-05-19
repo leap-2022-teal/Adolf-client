7e91c5ca443f0a7379c4522e501166ddb8ba2bf8
Namuun <mit.irnitu@gmail.com>
2023-05-19 21:58:34 +0800

    deleted consoles

--- src/context/orderProvider.tsx
+++ src/context/orderProvider.tsx
@@ -2,19 +2,14 @@ import { createContext, useState } from 'react';
 
 export const OrderContext = createContext<any>(undefined);
 export function OrderProvider({ children }: any) {
-  // const [order, setOrder] = useState<any>(undefined);
   const [serviceOrder, setServiceOrder] = useState<any>([]);
   const [SPinfo, setSPinfo] = useState<any>([]);
-  console.log('selectedService', serviceOrder);
-  console.log('spInfoshd', SPinfo);
   function addToOrder(selectedServiceInfo: any) {
     setServiceOrder(selectedServiceInfo);
   }
   const [UserselectedDate, setSelectedDate] = useState<any>(undefined);
-  // console.log('ppppeee', UserselectedDate);
   function setUserSelectedDate(date: any) {
     setSelectedDate(date);
-    // console.log(date);
   }
   console.log(UserselectedDate, 'user date');
   return (
