2a8020fbaf9ddab4e13e7608819d7262909a75a0
enkhnairamdal <enkhnairamdalbatmagnai@gmail.com>
2023-05-25 20:51:35 +0800

    guestLogin

---
+++ src/components/guestLogin.tsx
@@ -0,0 +1,61 @@
+import axios from 'axios';
+
+export default function GuestLogin() {
+  const userInput = {
+    phoneNumber: Math.floor(Math.random() * 100000000),
+    password: '1',
+  };
+  async function handleRegister(e: any) {
+    e.preventDefault();
+    await axios
+      .post(
+        `${process.env.NEXT_PUBLIC_REACT_APP_API_URL}/user/registration`,
+        userInput
+      )
+      .then((res) => {
+        console.log(res);
+        const { status, data } = res;
+        console.log(status);
+        if (status === 201 || status === 200) {
+          axios
+            .post(`${process.env.NEXT_PUBLIC_REACT_APP_API_URL}/user/login`, {
+              phoneNumber: userInput.phoneNumber,
+              password: userInput.password,
+            })
+            .then((res) => {
+              const { data, status } = res;
+              console.log(status);
+              if (status === 201 || status === 200) {
+                const { token, userId } = data;
+                localStorage.setItem('loginToken', token);
+                alert('Амжилттай нэвтэрлээ');
+                window.location.reload();
+              } else {
+                alert(`Error: ${status}`);
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+              alert(err.response?.data.message);
+            });
+        }
+      })
+      .catch((err) => {
+        // else if (res.status === 400) {
+
+        console.log('ene', err.message);
+        alert(`${err.response.data.message}`);
+        // }
+      });
+    console.log(userInput);
+  }
+
+  return (
+    <button
+      className="text-white h-[2.5rem] w-[80%] bg-blue-600  rounded-[10px] cursor-pointer"
+      onClick={handleRegister}
+    >
+      Зочиноор нэвтрэх
+    </button>
+  );
+}
